eaaf5da8423a655246a953afc3ac161c54778d0a
ErickPimentel <erickspinellipimentel@gmail.com>
2019-11-11 17:38:29 -0300

    adiciona novas mudanças para testes

--- projmicro/projmicro.ino
+++ projmicro/projmicro.ino
@@ -1,14 +1,14 @@
 #include <Ultrasonic.h>
 
-const int A1AD = 0;
-const int A1BD = 1;
-const int B1AD = 2;
-const int B1BD = 3;
+const int A1AD = 2;
+const int A1BD = 3;
+const int B1AD = 4;
+const int B1BD = 5;
 
-const int A1AE = 4;
-const int A1BE = 5;
-const int B1AE = 6;
-const int B1BE = 7;
+const int A1AE = 6;
+const int A1BE = 7;
+const int B1AE = 8;
+const int B1BE = 9;
 
 void setup() {
   pinMode(B1AD,OUTPUT);
@@ -27,54 +27,54 @@ void setup() {
 
 void motorA(char d){
   if(d =='F'){
-    digitalWrite(A1AD,LOW);
-    digitalWrite(A1BD,HIGH); 
+    analogWrite(A1AD,0);
+    analogWrite(A1BD,100); 
   }else if (d =='B'){
-    digitalWrite(A1AD,HIGH);
-    digitalWrite(A1BD,LOW);    
+    analogWrite(A1AD,100);
+    analogWrite(A1BD,0);    
   }else{
-    digitalWrite(A1AD,LOW);
-    digitalWrite(A1BD,LOW);    
+    analogWrite(A1AD,0);
+    analogWrite(A1BD,0);    
   }
 }
 
 void motorB(char d){
     if(d =='F'){
-      digitalWrite(B1AD,LOW);
-      digitalWrite(B1BD,HIGH); 
+      analogWrite(B1AD,0);
+      analogWrite(B1BD,150); 
     }else if(d =='B'){
-      digitalWrite(B1AD,HIGH);
-      digitalWrite(B1BD,LOW);    
+      analogWrite(B1AD,150);
+      analogWrite(B1BD,0);    
     }else{      
-      digitalWrite(B1AD,LOW);
-      digitalWrite(B1BD,LOW);     
+      analogWrite(B1AD,0);
+      analogWrite(B1BD,0);     
     }
 
 }
 
 void motorC(char d){
   if(d =='B'){
-    digitalWrite(A1AE,LOW);
-    digitalWrite(A1BE,HIGH); 
+    analogWrite(A1AE,0);
+    analogWrite(A1BE, 150); 
   }else if (d =='F'){
-    digitalWrite(A1AE,HIGH);
-    digitalWrite(A1BE,LOW);    
+    analogWrite(A1AE,150);
+    analogWrite(A1BE,0);    
   }else{
-    digitalWrite(A1AE,LOW);
-    digitalWrite(A1BE,LOW);    
+    analogWrite(A1AE,0);
+    analogWrite(A1BE,0);    
   }
 }
 
 void motorD(char d){
     if(d =='B'){
-      digitalWrite(B1AE,LOW);
-      digitalWrite(B1BE,HIGH); 
+      analogWrite(B1AE,0);
+      analogWrite(B1BE,150); 
     }else if(d =='F'){
-      digitalWrite(B1AE,HIGH);
-      digitalWrite(B1BE,LOW);    
+      analogWrite(B1AE,150);
+      analogWrite(B1BE,0);    
     }else{      
-      digitalWrite(B1AE,LOW);
-      digitalWrite(B1BE,LOW);     
+      analogWrite(B1AE,0);
+      analogWrite(B1BE,0);     
     }
 
 }
@@ -113,10 +113,12 @@ void left(){
 }
 
 void loop() {
-  front();
-  back();
-  right();
-  left();
+  motorA('F');
+  /*motorB('F'); 
+  motorC('F');
+  motorD('F');
+  delay(2000);*/
 }
+
 
 

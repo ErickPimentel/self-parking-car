f41088bc92b519476703a1de20666226086ed862
ErickPimentel <erickspinellipimentel@gmail.com>
2019-11-13 10:30:35 -0300

    remove arquivos desnecessario

--- projmicro/projmicro.ino
+++ projmicro/projmicro.ino
@@ -1,124 +1,41 @@
-#include <Ultrasonic.h>
+// output motors ports
+const int MOTOR_A_0 = 2;
+const int MOTOR_A_1 = 3;
+const int MOTOR_B_0 = 4;
+const int MOTOR_B_1 = 5;
+const int MOTOR_C_0 = 6;
+const int MOTOR_C_1 = 7;
+const int MOTOR_D_0 = 8;
+const int MOTOR_D_1 = 9.;
 
-const int A1AD = 2;
-const int A1BD = 3;
-const int B1AD = 4;
-const int B1BD = 5;
+const int motors_ports[8] = {
+  MOTOR_A_0, MOTOR_A_1, 
+  MOTOR_B_0, MOTOR_B_1,
+  MOTOR_C_0, MOTOR_C_1,
+  MOTOR_D_0, MOTOR_D_1
+};
 
-const int A1AE = 6;
-const int A1BE = 7;
-const int B1AE = 8;
-const int B1BE = 9;
 
-void setup() {
-  pinMode(B1AD,OUTPUT);
-  pinMode(B1BD,OUTPUT);
-  
-  pinMode(A1AD,OUTPUT);
-  pinMode(A1BD,OUTPUT);
-  
-  pinMode(B1AE,OUTPUT);
-  pinMode(B1BE,OUTPUT);
-  
-  pinMode(A1AE,OUTPUT);
-  pinMode(A1BE,OUTPUT);
-  delay(3000);
-}
-
-void motorA(char d){
-  if(d =='F'){
-    analogWrite(A1AD,0);
-    analogWrite(A1BD,100); 
-  }else if (d =='B'){
-    analogWrite(A1AD,100);
-    analogWrite(A1BD,0);    
-  }else{
-    analogWrite(A1AD,0);
-    analogWrite(A1BD,0);    
+void setup(){
+  // setup motors ports to output
+  for (int i = 0; i < 8; i++){
+    pinMode(motors_ports[i], OUTPUT);
   }
-}
+        
+        pinMode(4, OUTPUT);
+        pinMode(5, OUTPUT);
+      
 
-void motorB(char d){
-    if(d =='F'){
-      analogWrite(B1AD,0);
-      analogWrite(B1BD,150); 
-    }else if(d =='B'){
-      analogWrite(B1AD,150);
-      analogWrite(B1BD,0);    
-    }else{      
-      analogWrite(B1AD,0);
-      analogWrite(B1BD,0);     
-    }
-
-}
-
-void motorC(char d){
-  if(d =='B'){
-    analogWrite(A1AE,0);
-    analogWrite(A1BE, 150); 
-  }else if (d =='F'){
-    analogWrite(A1AE,150);
-    analogWrite(A1BE,0);    
-  }else{
-    analogWrite(A1AE,0);
-    analogWrite(A1BE,0);    
-  }
-}
-
-void motorD(char d){
-    if(d =='B'){
-      analogWrite(B1AE,0);
-      analogWrite(B1BE,150); 
-    }else if(d =='F'){
-      analogWrite(B1AE,150);
-      analogWrite(B1BE,0);    
-    }else{      
-      analogWrite(B1AE,0);
-      analogWrite(B1BE,0);     
-    }
-
-}
-
-void front(){
-  motorA('F');
-  motorB('F'); 
-  motorC('F');
-  motorD('F');
-  delay(2000);
+//      digitalWrite(MOTOR_B_0, HIGH);
+//      digitalWrite(MOTOR_B_1, LOW);
+// digitalWrite(MOTOR_B_1, LOW);
+      //digitalWrite(MOTOR_A_0, HIGH);
+      //digitalWrite(MOTOR_A_1, LOW);
+      
 }
 
 
-void back(){
-  motorA('B');
-  motorB('B'); 
-  motorC('B');
-  motorD('B');
-  delay(2000);
+void loop(){
+         analogWrite(4, 0);
+        analogWrite(5, 250);
 }
-
-void right(){
-  motorA('B');
-  motorB('B'); 
-  motorC('F');
-  motorD('F');
-  delay(2000);
-}
-
-void left(){
-  motorA('F');
-  motorB('F'); 
-  motorC('B');
-  motorD('B');
-  delay(2000);
-}
-
-void loop() {
-  motorA('F');
-  /*motorB('F'); 
-  motorC('F');
-  motorD('F');
-  delay(2000);*/
-}
-
-
-

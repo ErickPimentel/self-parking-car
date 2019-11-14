b9b6e0e726b4ae8bf882afb0f0f2a3dc27a419c4
ErickPimentel <erickspinellipimentel@gmail.com>
2019-11-14 10:47:13 -0300

    modifica arquivo principal

--- projmicro/projmicro.ino
+++ projmicro/projmicro.ino
@@ -6,7 +6,9 @@ const int MOTOR_B_1 = 5;
 const int MOTOR_C_0 = 6;
 const int MOTOR_C_1 = 7;
 const int MOTOR_D_0 = 8;
-const int MOTOR_D_1 = 9.;
+const int MOTOR_D_1 = 9;
+
+const int POWER_BUTTON = 0; 
 
 const int motors_ports[8] = {
   MOTOR_A_0, MOTOR_A_1, 
@@ -16,13 +18,24 @@ const int motors_ports[8] = {
 };
 
 
+
+// controls the execution of the code
+bool device_power = true;
+
+
 void setup(){
+  // sets motors pin mode
   for (int i = 0; i < 8; i++){
     pinMode(motors_ports[i], OUTPUT);
-  }      
+  }
+
+  // set button pin mode
+  pinMode(POWER_BUTTON, INPUT);
+
+  Serial.begin(9600);
 }
 
 
-void loop(){
-
+void loop(){  
+  
 }
